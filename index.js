50907a6f769af4dc45a2afc6e4d2370ff87e0714
Abubakar50C <abubakara2005@gmail.com>
2023-11-15 08:45:27 -0500

    small tweaks mainly to html template

--- index.js
+++ index.js
@@ -1,47 +1,30 @@
+
 // TODO: Include packages needed for this application
-const Inquirer = require('inquirer')
+const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'What is your project title?',
-      name: 'title',
-    },
-    {
-      type: 'input',
-      message: 'Type a short description of your project.',
-      name: 'description',
-    },
-    {
-      type: 'input',
-      message: 'What are the steps needed to install your application?',
-      name: 'installation',
-    },
-    
-  ])
-  .then((response) => {
-    
-    fs.writeFile('./readme.html', writeToFile(response), err => {
-        if (err) {
-          console.error(err);
-          
-       }
-        console.log('Readme file successfully created')
-       
-      });
-      console.log('Your new readme has been created!')
-  }
-  
-)
+  {
+    type: 'input',
+    message: 'What is your project title?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Type a short description of your project.',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'What are the steps needed to install your application?',
+    name: 'installation',
+  },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(title, description, installation) {
-    `<!DOCTYPE html>
+  return `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8">
@@ -53,25 +36,32 @@ function writeToFile(title, description, installation) {
   </head>
   <body>
     <main>
-        <h1 class= 'flex justify-center  border-b-1'># <${title}></h1>  
-        <div class= 'bg-blue-200 border-2 border-dashed'>
-            <h2 class= 'flex justify-center text-lg text-bold border-b-1 '>##DESCRIPTION</h2>
-            <h2 class= 'flex justify-center  border-b-1 '> ${description}.</h2>
-            <h2 class= 'flex justify-center  border-b-1 '> ## INSTALLATION</h2>
-            <h2 class= 'flex justify-center  border-b-1 '> ${installation}.</h2>
+        <h1 class='flex justify-center  border-b-1'># ${title}</h1>  
+        <div class='bg-blue-200 border-2 border-dashed'>
+            <h2 class='flex justify-center text-lg text-bold border-b-1 '>##DESCRIPTION</h2>
+            <h2 class='flex justify-center  border-b-1 '> ${description}.</h2>
+            <h2 class='flex justify-center  border-b-1 '> ## INSTALLATION</h2>
+            <h2 class='flex justify-center  border-b-1 '> ${installation}.</h2>
         </div>
     </main>
-	
   </body>
 </html>`;
-    
-      }
-
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    questions();
+  inquirer
+    .prompt(questions)
+    .then((response) => {
+      fs.writeFile('./readme.html', writeToFile(response.title, response.description, response.installation), (err) => {
+        if (err) {
+          console.error(err);
+        }
+        console.log('Readme file successfully created');
+      });
+    })
+    .catch((err) => console.error(err));
 }
 
-// Function call to initialize app
+// Call the init function to start the application
 init();
